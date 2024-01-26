3d58aed194993d9d76ee685d08d077d805de84dc
Tedy Yohanes <91521891+tedy97123@users.noreply.github.com>
2024-01-25 18:04:55 -0800

    its better

--- server.js
+++ server.js
@@ -7,23 +7,27 @@ const app = express();
 const port = 3000;
 
 // HTTP API Access
+var HttpsProxyAgent = require('https-proxy-agent');
 var request = require('request');
+var proxy = "http://dz5bgkpot6vs7f:edymozx5jpa4uypp02kova71n@us-east-static-04.quotaguard.com:9293";
+var agent = new HttpsProxyAgent(proxy);
+request({
+	uri: "https://example.com/api",
+	method: "POST",
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded'
+	},
+	agent: agent,
+	timeout: 10000,
+	followRedirect: true,
+	maxRedirects: 10,
+	body: "name=john"
+}, function(error, response, body) {
+	console.log("Error" + error);
+	console.log("Response: " + response);
+	console.log("Body: " + body);
+});
 
-var options = {
-    proxy: 'http://dz5bgkpot6vs7f:edymozx5jpa4uypp02kova71n@us-east-static-04.quotaguard.com:9293',
-    url: 'https://api.github.com/repos/joyent/node',
-    headers: {
-        'User-Agent': 'node.js'
-    }
-};
-
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
-    }
-}
-
-request(options, callback);
  
 // Express Server
 app.get('/', (req, res) => {
